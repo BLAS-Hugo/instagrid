df5f88390fba6ac24a7b5001417f5bc795ec9627
Hugo Blas <hugo.blas@outlook.fr>
2023-11-16 22:37:50 +0100

    Mise en place des contraintes pour les vues portrait et paysage

---
+++ InstaGrid/View/LayoutImageView.swift
@@ -0,0 +1,20 @@
+//
+//  LayoutImageView.swift
+//  InstaGrid
+//
+//  Created by Hugo Blas on 09/11/2023.
+//
+
+import UIKit
+
+class LayoutImageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
